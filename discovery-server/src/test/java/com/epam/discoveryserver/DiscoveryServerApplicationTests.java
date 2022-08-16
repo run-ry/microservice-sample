c5671a02a4c60493674238a70913eb6f0787aa6e
Run Ry <Run.Ry@jtrustroyal.com>
2022-08-16 14:27:06 +0700

    add discovery and config server

---
+++ discovery-server/src/test/java/com/epam/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrun.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
